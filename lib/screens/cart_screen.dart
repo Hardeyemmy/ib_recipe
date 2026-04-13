f9f98a0cf5f406260e2e7ec62fe0859e253bbf31
Hardeyemmy <iiadeyemi307@gmail.com>
2026-04-13 17:06:01 +0100

    mobile screen debugging ongoing

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../app_state.dart';
-import 'checkout_screen.dart';
 import 'recipe_homescreen.dart';
 
 class CartPage extends StatelessWidget {
--- lib/screens/checkout_screen.dart
+++ lib/screens/checkout_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ib_recipe/screens/cart_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -216,7 +217,7 @@ class _CheckoutPageState extends State<CheckoutPage> {
           if (mounted) {
             Navigator.pushAndRemoveUntil(
               context,
-              MaterialPageRoute(builder: (_) => const RecipeHomeScreen()),
+              MaterialPageRoute(builder: (_) => const CartPage()),
               (route) => false,
             );
           }
